3c5a4a48e071d3ad781f0bb6aab4e641da491f6a
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-20 22:06:35 -0300

    Criando rota para teste

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ require('dotenv').config();
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (_req, resp) => {
+  resp.status(200).json({ message: 'Ola mundo' });
+});
+
 const { PORT } = process.env || 3000;
 
 app.listen(PORT, () => console.log(`Servidor iniciou na porta ${PORT}`));
