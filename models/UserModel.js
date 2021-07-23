25daac2c5d77051a29bd62435c8a6b46e541a319
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-22 22:03:10 -0300

    Removendo linha desnecessaria

--- models/UserModel.js
+++ models/UserModel.js
@@ -3,7 +3,6 @@ const connection = require('./connection');
 const createUserBank = async (name, email, password) => {
   try {
     const userCreate = await connection().then((db) => db.collection('users').insertOne({ name, email, password }));
-    console.log(userCreate);
     return userCreate.acknowledged;
   } catch (err) {
     console.log(err);
