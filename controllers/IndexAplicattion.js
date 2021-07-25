fb96ed747aae129d6b7138edc0b1df7b2af2f8a9
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-25 14:29:44 -0300

    Removendo controller e router index aplication

--- controllers/IndexAplicattion.js
+++
@@ -1,7 +0,0 @@
-const userIndex = (_req, resp) => {
-  resp.status(200).json({ message: 'Rota /' });
-};
-
-module.exports = {
-  userIndex,
-};
--- routers/IndexAplicattion.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const IndexAplicattion = require('../controllers/IndexAplicattion');
-
-router.get('/', IndexAplicattion.userIndex);
-
-module.exports = router;
